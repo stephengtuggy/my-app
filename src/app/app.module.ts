f0e2d9cee3afedb104bfcb18c5b27a176be61c80
stephengtuggy <sgt@stephengtuggy.com>
2017-05-24 18:42:18 -0700

    Generate the basic files for initial version of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+//import 'rxjs-extensions';
 
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+import { AnimalSearchComponent } from './animal-search/animal-search.component';
+import { AnimalService } from './animal.service';
+import { InMemoryDataService } from './in-memory-data.service';
+import { FoodDetailComponent } from './food-detail/food-detail.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodSearchComponent } from './food-search/food-search.component';
+import { FoodService } from './food-service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AnimalService,
+    FoodService
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AnimalDetailComponent,
+    AnimalListComponent,
+    AnimalSearchComponent,
+    FoodDetailComponent,
+    FoodListComponent,
+    FoodSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/food-search/food-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-search',
+  templateUrl: './food-search.component.html',
+  styleUrls: ['./food-search.component.css']
+})
+export class FoodSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
