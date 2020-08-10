6633a3e24144c14e0df4e3ebe6dac95c47900462
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-08-09 17:24:46 -0700

    src/app/app.component.spec.ts: Remove all references to 'My First...'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,13 +20,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Zoo Example'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('My First Angular CLI App');
+    expect(app.title).toEqual('Zoo Example');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('My First Angular CLI App');
+    expect(compiled.querySelector('h1').textContent).toContain('Zoo Example');
   }));
 });
