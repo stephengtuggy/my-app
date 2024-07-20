d8302ef12c66676c1b37c97ab4ce4f293e5ab364
Stephen G Tuggy <sgt@stephengtuggy.com>
2024-07-20 12:42:58 -0700

    app.module.ts: Formatting, cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import {importProvidersFrom, NgModule} from '@angular/core';
+import { importProvidersFrom, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
