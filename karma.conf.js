1103bbe438e8d276a3a052d153c77becb3546a30
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-02-25 08:09:33 -0800

    karma.conf.js: Switch from Firefox to FirefoxHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -39,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['FirefoxHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
