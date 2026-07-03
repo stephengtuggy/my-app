85dd82d08c616b4b711c93908d24b9d3d3de4215
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-07-03 06:51:36 -0700

    app-routing.module.ts: `ng generate @angular/core:route-lazy-loading`

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { AnimalDetailComponent } from '../animal-detail/animal-detail.component';
-import { AnimalListComponent } from '../animal-list/animal-list.component';
-import { FoodDetailComponent } from '../food-detail/food-detail.component';
-import { FoodListComponent } from '../food-list/food-list.component';
-
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'animals/:id', component: AnimalDetailComponent },
-  { path: 'animals', component: AnimalListComponent },
-  { path: 'foods/:id', component: FoodDetailComponent },
-  { path: 'foods', component: FoodListComponent }
+  { path: 'dashboard', loadComponent: () => import('../dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  { path: 'animals/:id', loadComponent: () => import('../animal-detail/animal-detail.component').then(m => m.AnimalDetailComponent) },
+  { path: 'animals', loadComponent: () => import('../animal-list/animal-list.component').then(m => m.AnimalListComponent) },
+  { path: 'foods/:id', loadComponent: () => import('../food-detail/food-detail.component').then(m => m.FoodDetailComponent) },
+  { path: 'foods', loadComponent: () => import('../food-list/food-list.component').then(m => m.FoodListComponent) }
 ];
 
 @NgModule({
