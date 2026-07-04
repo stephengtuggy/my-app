a5eacc358b83d04064b581f2a009514bb847d471
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-07-03 21:56:46 -0700

    WIP debugging test framework

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath, URL } from 'node:url';
 import { playwright } from '@vitest/browser-playwright';
+import { angular } from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
-    plugins: [],
+    plugins: [angular()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
