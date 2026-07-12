bafe77778a34fab9826755604dc15ec99f821f90
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-07-12 12:03:26 -0700

    animal.service.spec.ts, food.service.spec.ts: Combine `beforeEach` clauses, and adjust for vitest differences

--- src/app/food.service.spec.ts
+++ src/app/food.service.spec.ts
@@ -1,23 +1,18 @@
-import {inject, TestBed, waitForAsync} from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 import {afterEach, beforeEach, describe, expect, it} from "vitest";
 
 import {FoodService} from './food.service';
-import {HttpTestingController} from "@angular/common/http/testing";
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
 import {Type} from "@angular/core";
+import {provideHttpClient, withInterceptorsFromDi, withXhr} from "@angular/common/http";
 
 describe('FoodService', () => {
     let httpMock: HttpTestingController;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            imports: [],
-            providers: [FoodService]
-        });
-    }));
-
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [FoodService]
+            imports: [],
+            providers: [provideHttpClient(withXhr(), withInterceptorsFromDi()), provideHttpClientTesting(), FoodService]
         });
         httpMock = TestBed.inject<HttpTestingController>(HttpTestingController as Type<HttpTestingController>);
     });
