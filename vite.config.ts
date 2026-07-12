7477f5a5ce728e3d84c2923f882b829085ca3993
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-07-12 10:29:28 -0700

    vite.config.ts: Omit @analogjs/vite-plugin-angular

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath, URL } from 'node:url';
 import { playwright } from '@vitest/browser-playwright';
-import { angular } from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
-    plugins: [angular()],
+    plugins: [],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
